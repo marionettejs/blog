604c19ecf71bb8c5763870adfce2fae65319cae9
Artem Denysov <denysov.artem@gmail.com>
2016-08-06 00:18:40 +0300

    Update travis-runner.sh

--- travis-runner.sh
+++ travis-runner.sh
@@ -10,5 +10,5 @@ then
   git init
   git add .
   git commit -m "deploy"
-  git push --force --quiet "https://${GH_TOKEN}@github.com/denar90/blog.git" master:gh-pages > /dev/null 2>&1
+  git push --force --quiet "https://${GH_TOKEN}@github.com/marionettejs/blog.git" master:gh-pages > /dev/null 2>&1
 fi
