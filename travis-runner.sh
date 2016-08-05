7dcf56ccdfdbef65dbfc7009292e314ec710795e
denar90 <denysov.artem@gmail.com>
2016-08-05 23:46:00 +0300

    add autodeploy for blog

---
+++ travis-runner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+set -o pipefail
+
+echo "$TRAVIS_BRANCH"
+
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
+then
+  echo "Deploying!"
+  cd public
+  git init
+  git add .
+  git commit -m "deploy"
+  git push --force --quiet "https://${GH_TOKEN}@github.com/denar90/blog.git" master:gh-pages > /dev/null 2>&1
+fi
